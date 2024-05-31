6b79ed6f0c735874029a0705c4796873672d8c8a
tropers <tristan.ropers@gmail.com>
2024-05-31 10:44:20 +0200

    Add docstring to helper

--- parsing/helper.py
+++ parsing/helper.py
@@ -8,6 +8,9 @@ from parsing.schedule_status import ScheduleStatusEnum
 
 # Check if node has any non-successful schedules
 def check_non_successful_schedules(policy_domain: PolicyDomain, node: Node) -> bool:
+    """
+    Check if policy domain contains any non-successful backup runs.
+    """
     if policy_domain.has_non_successful_schedules:
         return True
 
