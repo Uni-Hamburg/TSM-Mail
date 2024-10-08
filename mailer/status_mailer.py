450cc779b2f8216a8fce68c53da8a49ec85d95bf
tropers <tristan.ropers@gmail.com>
2024-10-08 10:48:02 +0200

    Add mechanism to automatically reconnect to SMTP server after timeout / error

--- mailer/status_mailer.py
+++ mailer/status_mailer.py
@@ -4,6 +4,7 @@ status_mailer.py generates status mails from node and policy domain data
 
 import logging
 import smtplib
+from smtplib import SMTPException
 from email.message import EmailMessage
 
 from parsing.policy_domain import PolicyDomain
@@ -24,20 +25,36 @@ class StatusMailer:
     def __init__(self, smtp_host: str, smtp_port: int, template_path: str):
         self.__smtp_host = smtp_host
         self.__smtp_port = smtp_port
+        self.__smtp_conn = None
 
         # Load jinja2 mail HTML template
         self.__template = ReportTemplate(template_path)
 
         logger.debug("Connecting to %s at port %s...", self.__smtp_host, self.__smtp_port)
-        # Establish connection to the SMTP server
-        self.__smtp_conn = smtplib.SMTP(self.__smtp_host, self.__smtp_port)
-        self.__smtp_conn.starttls()
+
+    def __smtp_connected(self) -> bool:
+        """
+        Checks if the smtp connection is still connected or not.
+        """
+        if not self.__smtp_conn:
+            return False
+
+        try:
+            status = self.__smtp_conn.noop()[0]
+        except SMTPException:
+            status = -1
+        return status == 250
 
     def send_to(self, policy_domain: PolicyDomain, sender_addr: str, receiver_addr: str,
                 subject: str, replyto_addr: str, bcc_addr: str):
         """
         Renders the mail template and sends it using the smtp library.
         """
+        # Establish connection to the SMTP server if it is not established / timed out
+        if not self.__smtp_conn or not self.__smtp_connected():
+            self.__smtp_conn = smtplib.SMTP(self.__smtp_host, self.__smtp_port)
+            self.__smtp_conn.starttls()
+
         message = EmailMessage()
         message["Subject"] = subject
         message["From"] = sender_addr
